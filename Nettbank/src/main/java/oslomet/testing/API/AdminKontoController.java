17674aa3900eee3d00a271fe535fb6531c0813c2
BastienTest <73186901+BastienTest@users.noreply.github.com>
2022-02-01 15:51:34 +0100

    Initial commit

---
+++ Nettbank/src/main/java/oslomet/testing/API/AdminKontoController.java
@@ -0,0 +1,63 @@
+package oslomet.testing.API;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import oslomet.testing.DAL.AdminRepository;
+import oslomet.testing.Models.Konto;
+import oslomet.testing.Sikkerhet.Sikkerhet;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/adminKonto")
+public class AdminKontoController {
+    @Autowired
+    AdminRepository repository;
+
+    @Autowired
+    private Sikkerhet sjekk;
+
+    @GetMapping("/hentAlle")
+    public List<Konto> hentAlleKonti() {
+        String personnummer = sjekk.loggetInn();
+        if (personnummer!=null) {
+            return repository.hentAlleKonti();
+        }
+        return null;
+    }
+
+    @PostMapping("/registrer")
+    public String registrerKonto( Konto konto) {
+        String personnummer = sjekk.loggetInn();
+        if (personnummer!=null) {
+            String retur = repository.registrerKonto(konto);
+            return retur;
+        }
+        return "Ikke innlogget";
+    }
+
+    @PostMapping("/endre")
+    public String endreKonto(Konto konto) {
+        String personnummer = sjekk.loggetInn();
+        if (personnummer!=null) {
+            return repository.endreKonto(konto);
+        }
+        return "Ikke innlogget";
+    }
+
+    @GetMapping("/slett")
+    public String slettKonto(String kontonummer) {
+        String personnummer = sjekk.loggetInn();
+        if (personnummer!=null) {
+           return repository.slettKonto(kontonummer);
+        }
+        return "Ikke innlogget";
+    }
+    @Autowired
+    private DataSource dataSource;
+    @GetMapping("/initDB")
+    public String initDB(){
+        return repository.initDB(dataSource);
+    }
+}
